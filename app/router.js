b04ae6964387cefc328ed2cde0fb2a2c9d5fc9da
prabhu-1402 <Ammu@7904>
2021-01-27 18:44:40 +0530

    CSS Chnages Ember List View

--- app/router.js
+++ app/router.js
@@ -11,8 +11,8 @@ Router.map(function () {
   this.route('tab', {path:"tab/:tab_id"});
   this.route('all-settings');
   this.route('settings', {path:"settings/"});
-  this.route('settings', function() {
-    this.route('index', { path: '/index' });
-  })
-  this.route('settings', {path:"settings/:settings_path"});
+  // this.route('settings', function() {
+  //   this.route('index', { path: '/index' });
+  // })
+  // this.route('settings', {path:"settings/:settings_path"});
 });
